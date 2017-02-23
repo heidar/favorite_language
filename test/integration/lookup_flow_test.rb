e934671297280bac5328c8b08e0a10d3b7d32b0c
Heidar Bernhardsson <heidarbn@gmail.com>
2017-02-23 19:54:16 +0800

    refactor integration test

--- test/integration/lookup_flow_test.rb
+++ test/integration/lookup_flow_test.rb
@@ -2,39 +2,35 @@ require 'test_helper'
 
 class LookupFlowTest < Capybara::Rails::TestCase
   test 'lookup' do
-    visit root_path
-    fill_in 'Enter a GitHub Username', with: 'heidar'
-    click_on 'Lookup'
-
+    perform_lookup username: 'heidar'
     assert page.has_content?('Ruby')
   end
 
   test 'lookup validation only fired on submit' do
     visit root_path
-
     refute page.has_content?("Username can't be blank")
   end
 
   test 'lookup validation' do
-    visit root_path
-    click_on 'Lookup'
-
+    perform_lookup username: nil
     assert page.has_content?("Username can't be blank")
   end
 
   test 'lookup non-existent user' do
-    visit root_path
-    fill_in 'Enter a GitHub Username', with: 'kjsadhfksadjhfdkasjf'
-    click_on 'Lookup'
-
+    perform_lookup username: 'kjsadhfksadjhfdkasjf'
     assert page.has_content?('User not found')
   end
 
   test 'lookup user with no public repos' do
-    visit root_path
-    fill_in 'Enter a GitHub Username', with: 'johnc'
-    click_on 'Lookup'
-
+    perform_lookup username: 'johnc'
     assert page.has_content?('User has no repositories listed')
+  end
+
+  private
+
+  def perform_lookup(username:)
+    visit root_path
+    fill_in 'Enter a GitHub Username', with: username
+    click_on 'Lookup'
   end
 end
